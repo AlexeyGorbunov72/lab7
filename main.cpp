08e6f2fca400471d9b0a0b67296b626b6c67d45c
AnyDodic <korri.13@inbox.ru>
2020-05-07 09:51:32 +0300

    wspjvoawk

--- main.cpp
+++ main.cpp
@@ -7,20 +7,20 @@ void shit(int* some){
 
 }
 int main() {
-    CircularBuffer<int> ochko(4);
-    ochko.append(2213);
-    ochko.append(11);
+    CircularBuffer<int> buffer(4);
+    buffer.append(2213);
+    buffer.append(11);
 
 
 //    std::sort(ochko.getIterator().getHead(), ochko.getIterator().getTail());
 //    for (int i = 0; i < 4; ++i) {
-//        std::cout << ochko[i] << ' ';
+//        std::cout << buffer[i] << ' ';
 //    }
 
-    ochko.addFirst(3);
-    ochko.addFirst(123);
-    ochko.show();
+    buffer.addFirst(3);
+   
+    buffer.show();
     for (int i = 0; i < 4; ++i) {
-        std::cout << ochko[i] << ' ';
+        std::cout << buffer[i] << ' ';
     }
 }
