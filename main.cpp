20654ec085962d58c638c0099143dfe7809f2136
AnyDodic <korri.13@inbox.ru>
2020-05-08 16:08:57 +0300

    REBUILD

--- main.cpp
+++ main.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
 #include "HandMadeIterator.cpp"
 #include "CircularBuffer.cpp"
-void shit(int* some){
-    int* hui = &some[0];
-    std::cout << *hui;
-
-}
 int main() {
-    CircularBuffer<int> buffer(4);
-    buffer.append(2213);
-    buffer.append(11);
-
-
-//    std::sort(ochko.getIterator().getHead(), ochko.getIterator().getTail());
-//    for (int i = 0; i < 4; ++i) {
-//        std::cout << buffer[i] << ' ';
-//    }
-
-    buffer.addFirst(3);
-
-    buffer.show();
-    for (int i = 0; i < 4; ++i) {
-        std::cout << buffer[i] << ' ';
-    }
+   
 }
